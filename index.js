1683e56ad04429e31a375044deb7377ce1f1dde7
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-07-14 10:07:37 +0530

    completion API

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const coffees = require("./coffees.json");
 const oas = require("./oas.json");
 const manifest = require("./manifest.json");
 
+const axios = require('axios');
+const openai = require("openai");
+
 const app = express();
 
 const port = 4000;
@@ -18,6 +21,38 @@ app.get("/coffees/list", (req, res) => {
 
   res.json(result);
 });
+
+
+const api_key = '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';
+
+app.get("/completion" , async (req , res) => {
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+      // model: 'gpt-3.5-turbo',
+      model: "gpt-4",
+      temperature: 0.8,
+      max_tokens: 2000,
+      messages: [
+        { role: 'system', content: 'You are a poet who creates poems that evoke emotions.' },
+        { role: 'user', content: 'Write a short poem for programmers.' }
+      ]
+    }, {
+      headers: {
+        'Authorization': `Bearer ${api_key}`,
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const poem = response.data.choices[0].message.content;
+    res.status(200).json(poem);
+  } catch (error) {
+    console.error("Error:", error.response.data.error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+
+});
+
+// generatePoem();
 
 app.get("/coffees/details/:id", (req, res) => {
   const coffee = coffees.find((c) => c.id == req.params.id);
