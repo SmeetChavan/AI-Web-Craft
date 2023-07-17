8fdf2ccb38a3009501949cf846b7f831b97db740
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-07-17 16:04:29 +0530

    template.html

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 const { Configuration, OpenAIApi } = require("openai");
 
+
 dotenv.config({
   path: "./.env",
 });
@@ -19,6 +20,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+app.use(express.json());
 
 const port = 4000;
 
@@ -30,24 +32,24 @@ app.get("/coffees/list", (req, res) => {
       price: coffee.price,
     };
   });
-
+  
   res.json(result);
 });
 
 app.get("/image" , async (req , res) => {
-
+  
   let template = "smet";
-
+  
   try {
-
+    
     const response = await openai.createImage({
-      prompt: "Give me a barber shop website",
+      prompt: "Barber Shop",
       n: 1,
       size: "1024x1024",
     });
-
+    
     res.status(200).send(response.data.data[0].url);
-
+    
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Something went wrong" });
@@ -56,11 +58,11 @@ app.get("/image" , async (req , res) => {
 
 app.get("/coffees/details/:id", (req, res) => {
   const coffee = coffees.find((c) => c.id == req.params.id);
-
+  
   if (!coffee) {
     return res.status(404).json({ error: "Coffee not found" });
   }
-
+  
   res.json(coffee);
 });
 
@@ -72,6 +74,17 @@ app.get("/.well-known/ai-plugin.json", (req, res) => {
   res.json(manifest);
 });
 
+app.post("/assist" , async(req , res) => {
+
+  const {text} = req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo-16k-0613",
+    messages: [{"role": "system", "content": "You are a website builder"}, {role: "user", content: text}],
+  });
+
+  res.send(completion.data.choices[0].message);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
